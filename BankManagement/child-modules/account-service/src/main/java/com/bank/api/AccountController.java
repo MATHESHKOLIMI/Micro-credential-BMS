58a6316224cbdc4a9ea8a407e85b37d9a8e792d5
Joydev Das <iamjoydevdas@gmail.com>
2020-08-16 01:20:15 +0530

    Account service

--- BankManagement/child-modules/account-service/src/main/java/com/bank/api/AccountController.java
+++ BankManagement/child-modules/account-service/src/main/java/com/bank/api/AccountController.java
@@ -1,11 +1,13 @@
 package com.bank.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.feign.IAccountController;
 import com.bank.model.Account;
 import com.bank.model.ServiceResponse;
 
+@RestController
 public class AccountController implements IAccountController{
 
 	@Override
