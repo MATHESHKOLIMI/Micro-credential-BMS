311f76bbbf7d4d7572e33d31c31f78b7a276f348
Joydev Das <iamjoydevdas@gmail.com>
2020-08-15 15:13:34 +0530

    transaction banks

--- BankManagement/child-modules/transaction-service/src/main/java/com/bank/config/TransactionConfiguration.java
+++ BankManagement/child-modules/transaction-service/src/main/java/com/bank/config/TransactionConfiguration.java
@@ -6,12 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ComponentScan(basePackages= {
-								"com.bus",
-								"com.bus.transaction.api",
-								"com.bus.transaction.configuration",
-								"com.bus.transaction.service"
-		})
 public class TransactionConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
